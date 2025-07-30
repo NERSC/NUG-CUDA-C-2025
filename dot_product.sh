53b477ec71c49336a108819eba9d9bbf66be30b8
Charles Lively III, PhD <132687666+charlesWLivelyPhD@users.noreply.github.com>
2025-07-30 13:41:45 -0400

    Update dot_product.sh
    updates with reservation

--- dot_product.sh
+++ dot_product.sh
@@ -3,7 +3,9 @@
 #SBATCH -G 4 
 #SBATCH -t 5
 #SBATCH -C gpu 
-#SBATCH -A trn017
-#SBATCH --reservation=nug_cuda_c
+#SBATCH -A trn017 #for this training series for part 1 and part 2
+#SBATCH -q shared
+#SBATCH --reservation=nug_cuda_c #this line only during training until 2pm
+
 ./dot_product
 
