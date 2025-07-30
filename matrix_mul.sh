cb00a2e341fe9e8b6266dba84f905f153f0df8e1
Charles Lively III, PhD <132687666+charlesWLivelyPhD@users.noreply.github.com>
2025-07-30 13:43:15 -0400

    Update matrix_mul.sh

--- matrix_mul.sh
+++ matrix_mul.sh
@@ -3,7 +3,9 @@
 #SBATCH -G 4 
 #SBATCH -t 5
 #SBATCH -C gpu 
-#SBATCH -A m4388
+#SBATCH -A trn017 #for this training series for part 1 and part 2
+#SBATCH -q shared
+#SBATCH --reservation=nug_cuda_c #this line only during training until 2pm
 
 ./matrix_mul
 
